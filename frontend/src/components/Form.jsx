74073160592ff5e509fb48909761bcd346dd5301
Asib <ahmedasib1@gmail.com>
2025-04-11 01:07:34 +0600

    fix #6 : find out form url problem

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,49 +1,60 @@
-import React, { useState } from 'react';
+import { useState } from "react";
 import api from "../api";
-import { useNavigate } from 'react-router-dom';
-import { ACCESS_TOKEN, REFRESH_TOKEN } from '../constants';
+import { useNavigate } from "react-router-dom";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 
 
-const Form = ({route, method}) => {
-    const [userName, setUserName] = useState("");
+function Form({ route, method }) {
+    const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
 
-    const name = method === 'login' ? 'login' : 'register';
+    const name = method === "login" ? "Login" : "Register";
 
     const handleSubmit = async (e) => {
         setLoading(true);
         e.preventDefault();
-        console.log('[DEBUG] Base URL:', api.defaults.baseURL); 
-        try{
-            const res = await api.post(route, {userName, password});
-            console.log(res);
-            if(method === 'login'){
+
+        try {
+            const res = await api.post(route, { username, password })
+            if (method === "login") {
                 localStorage.setItem(ACCESS_TOKEN, res.data.access);
                 localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
-                navigate('/');
-            }else{
-                navigate('/login');
+                navigate("/")
+            } else {
+                navigate("/login")
             }
-        }catch (error){
-            console.error(error);
-            alert('Something went wrong');
-        }finally{
-            setLoading(false);
+        } catch (error) {
+            alert(error)
+        } finally {
+            setLoading(false)
         }
-
-    }
+    };
 
     return (
-        <form onSubmit={handleSubmit} className=''>
+        <form onSubmit={handleSubmit} className="form-container">
             <h1>{name}</h1>
-            <input type="text" value={userName} onChange={(e) => setUserName(e.target.value)} placeholder='User Name'/>
-            <input type="password" value={password} onChange={(e)=> setPassword(e.target.value)} placeholder='Enter your Password'/>
-            <button type='submit'>{name}</button>
+            <input
+                className="form-input"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Username"
+            />
+            <input
+                className="form-input"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+            />
+            {loading && <p>Loading...</p>}
+            <button className="form-button" type="submit">
+                {name}
+            </button>
         </form>
     );
-};
+}
 
-export default Form;
-
+export default Form
